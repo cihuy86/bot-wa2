8e72eb8c04dc90afa2892bbf380eb66cf1dbd488
cihuy86 <cahkroco11@gmail.com>
2026-07-06 23:10:48 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -2,7 +2,7 @@
 echo "📦 Menginstall dependencies untuk BOT WANGZ..."
 npm install --legacy-peer-deps
 if [ $? -eq 0 ]; then
-    echo "✅ Instalasi selesai! Jalankan bot dengan: ./run.sh"
+    echo "✅ Instalasi selesai! Jalankan bot dengan: ./run.sh atau node index.js"
 else
     echo "❌ Instalasi gagal. Coba lagi."
     exit 1
